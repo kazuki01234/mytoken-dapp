1ee406043a0991c203ea6a59774e18bf714108f0
kazuki01234 <188004350+kazuki01234@users.noreply.github.com>
2025-12-15 16:56:56 +0900

    Finalize staking integration and documentation

--- scripts/deploy_staking.js
+++ scripts/deploy_staking.js
@@ -1,21 +1,24 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-  const rewardRate = ethers.utils.parseEther("1");
-
   const Token = await ethers.getContractFactory("MyToken");
-  const token = await Token.deploy(ethers.utils.parseEther("1000000"));
+  const token = await Token.deploy(
+    ethers.utils.parseEther("1000000")
+  );
   await token.deployed();
 
   console.log("Token deployed to:", token.address);
 
   const Staking = await ethers.getContractFactory("Staking");
-  const staking = await Staking.deploy(token.address, rewardRate);
+  const staking = await Staking.deploy(token.address);
   await staking.deployed();
 
   console.log("Staking deployed to:", staking.address);
 
-  await token.transfer(staking.address, ethers.utils.parseEther("500000"));
+  await token.transfer(
+    staking.address,
+    ethers.utils.parseEther("500000")
+  );
   console.log("Reward tokens funded.");
 }
 
